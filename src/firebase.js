256558cd635a89befb60fe8c55b938bb4a28b8d7
wonho-jung <wonho.jung92@gmail.com>
2021-02-23 23:10:28 -0500

    basic setup before start

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzLh1g9eJdpIP_NT5Zh6hvCfbPZ19eSLA",
+  authDomain: "slack-clone-6f504.firebaseapp.com",
+  projectId: "slack-clone-6f504",
+  storageBucket: "slack-clone-6f504.appspot.com",
+  messagingSenderId: "276850236945",
+  appId: "1:276850236945:web:ebe3d358a0d6ae89bb5bb0",
+  measurementId: "G-S0N5S1DQVR",
+};
